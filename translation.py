19c46a10422da7c9ede73b5e724c95494e3b21bf
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-07-06 17:12:31 +0430

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,38 @@
+class Translation(object):
+    START_TEXT = """Hey! I'm Telegram Renaming BOT Programmed By <a href="https://tx.me/Hackintush">CɪᴘʜᴇʀX</a>
+<b>Please send me any Telegram file and reply to that file with /rename New Name.Format</b> 
+SEE /help For My Features 
+
+See /help for more Features.."""
+    RENAME_403_ERR = "There Is Some Error With The File" 
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = "Contact CɪᴘʜᴇʀX" 
+    DOWNLOAD_START = "Trying to Download ʘ‿ʘ "
+    UPLOAD_START = "Trying to Upload (ᵔᴥᵔ)"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thanks for Using Me 😄**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the File ᕙ(  • ‿ •  )ᕗ "
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully 😎"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """<b>How To Use Me</b> 
+    
+1. Send me any Telegram File.
+2. Reply to that message to /rename New Name.Format."""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.Format` with custom thumbnail support.."
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+The given file name has {num} characters.
+
+<b>Essays Not allowed in Telegram file name!</b>
+©️ <code>@ReNameBot</code>
+Please short your file name and try again!"""
